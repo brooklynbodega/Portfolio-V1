4296b2e44b631a7b83071066127b4f3ebb4cd91e
Raul Montero <raul.amontero@gmail.com>
2018-08-01 15:20:10 -0400

    added smooth scrolling

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-<script>
-function scrollTo(to, duration) {
-    if (document.body.scrollTop == to)
-      return;
-    let diff = to - document.body.scrollTop;
-    let scrollStep = Math.PI / (duration / 10);
-    let count = 0, currPos;
-    start = element.scrollTop;
-    scrollInterval = setInterval(function(){
-        if (document.body.scrollTop != to) {
-            count = count + 1;
-            currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
-            document.body.scrollTop = currPos;
-        }
-        else {
-          clearInterval(scrollInterval)
-        }
-    },10);
-}
+/* Code taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
 
-function test(elID)
-{
-    let dest = document.getElementById(elID);
-    scrollTo(dest.offsetTop, 500);
-}
-</script>
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
