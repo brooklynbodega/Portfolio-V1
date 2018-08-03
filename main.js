8c46843f1f89a9babd054b66d52c14c396e3acbf
Raul Montero <raul.amontero@gmail.com>
2018-08-02 21:39:03 -0400

    made sticky navbar

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+console.log("working");
+
+
+/* Code taken from: */
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () { myFunction() };
+
+// Get the header
+let nav = document.getElementById("nav");
+
+// Get the offset position of the navbar
+let sticky = nav.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
 /* Code taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
 
 // Select all links with hashes
@@ -12,7 +35,7 @@ $('a[href*="#"]')
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
-      var target = $(this.hash);
+      let target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       // Does a scroll target exist?
       if (target.length) {
@@ -23,7 +46,7 @@ $('a[href*="#"]')
         }, 1000, function () {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
+          let $target = $(target);
           $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
